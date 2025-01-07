6bc5135f19ee7e302c46a04bb925fad04b75f699
Utkarsh <utkjaiswal58@gmail.com>
2025-01-07 11:26:37 +0530

    function to check whether a particular player won or not

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ PLAYER_O = 'O'
 EMPTY = ' '
 
 class TicTacToe:
-    
+
     def __init__(self, mode='two-player'):
         self.board = [[EMPTY] * 3 for _ in range(3)]
         self.current_player = PLAYER_X
@@ -25,3 +25,38 @@ class TicTacToe:
             return True
         return False
 
+
+
+    def make_move(self, row, col):
+        if self.is_valid_move(row, col):
+            self.board[row][col] = self.current_player
+            return True
+
+        return False
+
+
+    def check_win(self):
+        for i in range(3):
+
+            if self.board[i][0] == self.board[i][1] == self.board[i][2] != EMPTY:
+                return True
+
+            if self.board[0][i] == self.board[1][i] == self.board[2][i] != EMPTY:
+                return True
+
+        if self.board[0][0] == self.board[1][1] == self.board[2][2] != EMPTY:
+            return True
+
+        if self.board[0][2] == self.board[1][1] == self.board[2][0] != EMPTY:
+            return True
+        
+        return False
+
+
+    def check_draw(self):
+        for row in self.board:
+            if EMPTY in row:
+                return False
+        return True
+
+    
