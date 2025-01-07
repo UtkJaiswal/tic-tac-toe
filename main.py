568b19b08bb06bbc3958f04693c84aeca5128e95
Utkarsh <utkjaiswal58@gmail.com>
2025-01-07 11:20:32 +0530

    function added to check if a move is valid or not

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ PLAYER_O = 'O'
 EMPTY = ' '
 
 class TicTacToe:
+    
     def __init__(self, mode='two-player'):
         self.board = [[EMPTY] * 3 for _ in range(3)]
         self.current_player = PLAYER_X
@@ -18,4 +19,9 @@ class TicTacToe:
 
         print("\n")
 
-        
+
+    def is_valid_move(self, row, col):
+        if 0 <= row < 3 and 0 <= col < 3 and self.board[row][col] == EMPTY:
+            return True
+        return False
+
